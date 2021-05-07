3b88a40e66b34a46bbf0192d45788385410d23a8
wei.peng <wei.peng@marcpoint.com>
2021-05-07 21:24:11 +0800

    版本7.0读写分离

--- ziface/imsghandler.go
+++ ziface/imsghandler.go
@@ -14,4 +14,10 @@ type IMsgHandle interface {
 
 	//为消息添加具体的处理逻辑
 	AddRouter(msgId uint32, router IRouter)
+
+	// 启动worker工作池
+	StartWorkerPool()
+
+	// 将消息交给taskQueue，有worker进行处理
+	SendMsgToTaskQueue(reader IRequest)
 }
